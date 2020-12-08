50f40d83dec8bbaf9813733cf7266d22da04e149
khzamighalia <elghaliakhzami@gmail.com>
2020-12-08 11:17:21 +0100

    added

--- src/com/employe/Employe.java
+++ src/com/employe/Employe.java
@@ -2,4 +2,29 @@ package com.employe;
 
 public class Employe {
 
+  private String nom;
+	private String prenom;
+	public Employe(String nom, String prenom) {
+		super();
+		this.nom = nom;
+		this.prenom = prenom;
+	}
+	public String getNom() {
+		return nom;
+	}
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	public String getPrenom() {
+		return prenom;
+	}
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+
+	public String toString() {
+		return " Nom=" + nom + ", prenom=" + prenom + " ";
+	}
+
+
 }
