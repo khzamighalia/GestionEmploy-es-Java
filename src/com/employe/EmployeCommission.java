1eb23d7db69e4c96e0e2c906e57c0f5876d7507a
jnoman <jamalnoman62@gmail.com>
2020-12-08 11:29:00 +0100

    ajouter class EmployeCommission

--- src/com/employe/EmployeCommission.java
+++ src/com/employe/EmployeCommission.java
@@ -1,5 +1,21 @@
 package com.employe;
 
-public class EmployeCommission {
+public class EmployeCommission extends Employe {
+	private double salaire;
+	private int ventes, commission;
 
+	public EmployeCommission(String nom, String prenom, double salaire, int ventes, int commission) {
+		super(nom, prenom);
+		this.salaire = salaire;
+		this.ventes = ventes;
+		this.commission = commission;
+	}
+	
+	public double calculSalaire() {
+		return salaire + (ventes * commission) / 100;
+	}
+	
+	public String toString() {
+		return super.toString() + ", Salaire total : " + calculSalaire();
+	}
 }
