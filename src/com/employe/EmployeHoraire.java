a6cf831169febcfe6eac0ae45a195044e383b705
khzamighalia <elghaliakhzami@gmail.com>
2020-12-08 11:51:08 +0100

    Employe horaire added

--- src/com/employe/EmployeHoraire.java
+++ src/com/employe/EmployeHoraire.java
@@ -1,5 +1,24 @@
 package com.employe;
 
-public class EmployeHoraire {
+public class EmployeHoraire extends Employe {
+	private double taux_horaire;
+	private int heures;
+	
+	public EmployeHoraire(String nom, String prenom, double taux_horaire, int heures) {
+		super(nom, prenom);
+		this.taux_horaire = taux_horaire;
+		this.heures = heures;
+	}
+
+	@Override
+	public String toString() {
+		return super.toString()+ " Salaire : "+calculSalaire()+ " Dhs ";
+	}
+
+
+	public double calculSalaire() {
+		return taux_horaire*heures;
+	}
+
 
 }
