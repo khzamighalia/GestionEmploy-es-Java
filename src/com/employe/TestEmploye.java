27e52663e07d20ed7479add7a59c0735e86742ca
jnoman <jamalnoman62@gmail.com>
2020-12-08 11:56:58 +0100

    ajouter class TestEmploye avec calss EmployeHoraire

--- src/com/employe/TestEmploye.java
+++ src/com/employe/TestEmploye.java
@@ -5,7 +5,7 @@ public class TestEmploye {
 	public static void main(String[] args) {
 		EmployeFixe employeFixe = new EmployeFixe("nom1", "prenom1", 5000);
 		EmployeCommission employeCommission =new EmployeCommission("nom2", "prenom2", 4000, 24000, 10);
-		EmployeHoraire employeHoraire = new EmployeHoraire("nom1", "prenom1", 30, 150);
+		EmployeHoraire employeHoraire = new EmployeHoraire("nom3", "prenom3", 30, 150);
 		
 		System.out.println(employeFixe.toString());
 		System.out.println(employeCommission.toString());
